4889abf833b7ed825fb7cf76297817c838aeadac
GergelyRoka <50467112+GergelyRoka@users.noreply.github.com>
2022-08-23 11:42:39 +0200

    Update ft_list_push_front.c
    plus parenthesis for the if condition

--- exercises/ex01/ft_list_push_front.c
+++ exercises/ex01/ft_list_push_front.c
@@ -4,7 +4,7 @@ void	ft_list_push_front(t_list **begin_list, void *data)
 {
 	t_list	*element;
 
-	if(element = ft_create_elem(data))
+	if((element = ft_create_elem(data)))
 	{
 		element -> next = *begin_list;
 		*begin_list = element;
